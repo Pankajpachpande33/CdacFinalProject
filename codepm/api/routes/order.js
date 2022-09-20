601c35238879c5cef6e77f5bb931b53289dcb074
Anand Patil <anand96patil@gmail.com>
2022-09-20 18:45:56 +0530

    deleted

--- codepm/api/routes/order.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAll, save } from '../controller/order.js';
-
-const routes = express.Router();
-
-routes.get("/", getAll);
-routes.post("/", save);
-
-export default routes;
--- codepm/client/src/constants/ApplicationTypes.js
+++
@@ -1,6 +0,0 @@
-export const START_LOADING = "START_LOADING";
-export const STOP_LOADING = "STOP_LOADING";
-
-
-//category
-export const LOAD_CATEGORY_SUCCESS = "LOAD_CATEGORY_SUCCESS";
